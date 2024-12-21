23f61899826fea41485067292d74cddcd16e2811
Miscanthus53 <s.yutaka1985@gmail.com>
2024-12-21 10:21:35 +0900

    Delete kadai_03/samuraitravel directory

--- kadai_03/samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51QUjQBG4NAbsrbJ8RXJ7aZ4tzkUTT00n1eIsffvG3ZqKw6z4yjH1dUeHM5UWYaTkiXkSilXOcxy8NSYqv8KnHVT800qZ56Jb2u');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
-  stripe.redirectToCheckout({
-    sessionId: sessionId
-  })
-});
